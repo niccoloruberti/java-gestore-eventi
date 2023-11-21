e83452c75fc76286f4f9bd71c5bc2373be25f210
niccoloruberti <nikiruberti@gmail.com>
2023-11-21 11:45:14 +0100

    milestone 2 completata

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -1,6 +1,7 @@
 package org.lessons.java;
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 import org.lessons.java.pojo.Evento;
 
@@ -8,19 +9,115 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		
+		Scanner in = new Scanner(System.in);
+		
 		Evento e1 = null;
 		
-		try {
+		System.out.println("Vuoi inserire un nuovo evento?");
+		
+		String ansUser = in.nextLine();
+		
+		if (ansUser.equals("si")) {
 			
-			e1 = new Evento("Concerto Lil Texas", LocalDate.parse("2023-12-25"), 100);
+			System.out.println("Inserisci il nome dell'evento: ");
 			
-		} catch (Exception e) {
+			String title = in.nextLine();
 			
-			System.err.println(e.getMessage());
+			System.out.println("Inserisci la data dell'evento: (yyyy-mm-dd) ");
+			
+			String date = in.nextLine();
+			
+			System.out.println("Inserisci il numero di posti disponiibili: ");
+			
+			String postiStr = in.nextLine();
+			
+			int posti = Integer.valueOf(postiStr);
+			
+			try {
+				
+				e1 = new Evento(title, LocalDate.parse(date), posti);
+				
+				System.out.println(e1);
+				
+			} catch (Exception e) {
+				
+				System.err.println(e.getMessage());
+				
+			}
+			
+		} else {
+			
+			in.close();
+			return;
+		}
+		
+		//prenotazione posti
+		
+		System.out.println("Vuoi prenotare questo evento? si/no");
+		
+		ansUser = in.nextLine();
+		
+		if (ansUser.equals("si")) {
+			
+			//svolgimento della prenotazione
+			System.out.println("Quanti posti vuoi prenotare?");
+			
+			String nPrenotazioniStr = in.nextLine();
+			
+			int nPrenotazioni = Integer.valueOf(nPrenotazioniStr);
+			
+			for (int x = 0; x < nPrenotazioni; x++) {
+				
+				try {
+					
+					e1.prenota();
+					
+				} catch (Exception e) {
+					
+					System.err.println(e.getMessage());
+				}
+				
+			}
+			
+			System.out.println("Posti totali prenotati: " + e1.getPostiPrenotati());
+			System.out.println("Posti disponibili: " + (e1.getPostiTotali() - e1.getPostiPrenotati()));
 			
 		}
 		
-		System.out.println(e1);
+		//annullare prenotazione
+		
+		System.out.println("Vuoi disidire una prenotazione? si/no");
+		
+		ansUser = in.nextLine();
+		
+		if (ansUser.equals("si")) {
+			
+			System.out.println("Quanti posti vuoi disdire? ");
+			
+			String nPrenotazioniAnnullateStr = in.nextLine();
+			
+			int nPrenotazioniAnnullate = Integer.valueOf(nPrenotazioniAnnullateStr);
+			
+			for (int x = 0; x < nPrenotazioniAnnullate; x++) {
+				
+				try {
+					
+					e1.disdici();
+					
+				} catch (Exception e) {
+					
+					System.err.println(e.getMessage());
+					break;
+					
+				}
+			}
+			
+			System.out.println("Posti totali prenotati: " + e1.getPostiPrenotati());
+			System.out.println("Posti disponibili: " + (e1.getPostiTotali() - e1.getPostiPrenotati()));
+			
+		}
+		
+		in.close();
 		
 	}
 
